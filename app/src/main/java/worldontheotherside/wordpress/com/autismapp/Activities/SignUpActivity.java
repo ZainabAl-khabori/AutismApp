27f421b69d1526fe4438d6c8342079fe373ac7df
Zainab <zainab.yazahraa@gmail.com>
2018-02-26 15:03:46 +0400

    -

---
+++ app/src/main/java/worldontheotherside/wordpress/com/autismapp/Activities/SignUpActivity.java
@@ -0,0 +1,15 @@
+package worldontheotherside.wordpress.com.autismapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import worldontheotherside.wordpress.com.autismapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/worldontheotherside/wordpress/com/autismapp/Fragments/PersonalInfoFragment.java
@@ -0,0 +1,8 @@
+package worldontheotherside.wordpress.com.autismapp.Fragments;
+
+/**
+ * Created by زينب on 2/25/2018.
+ */
+
+public class PersonalInfoFragment {
+}
