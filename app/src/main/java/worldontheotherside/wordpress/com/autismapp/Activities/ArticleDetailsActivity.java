c9831c4740852e0ed34b89030d065fa9b99618f5
AlMutasim <al-khanjar@hotmail.com>
2018-03-21 14:09:54 +0400

    +add ArticleDetailsActivity Class  and article_details_activity xml

--- app/src/main/java/worldontheotherside/wordpress/com/autismapp/Activities/ArticleDetailsActivity.java
+++ app/src/main/java/worldontheotherside/wordpress/com/autismapp/Activities/ArticleDetailsActivity.java
@@ -1,7 +1,10 @@
 package worldontheotherside.wordpress.com.autismapp.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import worldontheotherside.wordpress.com.autismapp.R;
@@ -14,6 +17,20 @@ public class ArticleDetailsActivity extends AppCompatActivity {
         setContentView (R.layout.article_details_activity);
 
         TextView tvToolbarTitle = findViewById (R.id.tv_search_title);
+
+        final EditText etComment = findViewById (R.id.et_comment);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById (R.id.iv_add_comment);
+        fab.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View view) {
+              /*  Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction ("Action", null).show ();*/
+                if (etComment.getVisibility () == View.GONE) {
+                    etComment.setVisibility (View.VISIBLE);
+                }
+            }
+        });
 
     }
 }
